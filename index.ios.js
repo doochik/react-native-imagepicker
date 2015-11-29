3ccb4b548af590734d8278292e2d36672f08afd4
Aleksei Androsov <doochik@ya.ru>
2015-11-29 14:49:15 +0300

    Button configurations

--- index.ios.js
+++ index.ios.js
@@ -5,13 +5,28 @@ var {
     ActionSheetIOS,
     CameraRoll,
     ImagePickerIOS
-    } = React;
+} = React;
 
 var ImagePicker = {
 
     /**
-     * Open ActionSheetIOS to select further action (take photo, choose last photo, choose from library).
+     * Opens ActionSheetIOS to select further action (take photo, choose last photo, choose from library).
+     *
      * @param {object} config
+     * @param {string} [config.cancelTitle="Cancel"] Cancel button title
+     *
+     * @param {object|boolean|string} [config.takePhoto] "Take photo" button config. False to disable this button.
+     * @param {string} [config.takePhoto.title="Take photo"] Button title.
+     * @param {string} [config.takePhoto.config={}] Config objects passed to ImagePickerIOS.openCameraDialog().
+     *
+     * @param {object|boolean|string} [config.useLastPhoto] "Use last photo" button config. False to disable this button.
+     * @param {string} [config.useLastPhoto.title="Take photo"] Button title.
+     * @param {object} [config.useLastPhoto.config={first: 1}] Config objects passed to CameraRoll.getPhotos().
+     *
+     * @param {object|boolean|string} [config.chooseFromLibrary] "Choose from library" button config. False to disable this button.
+     * @param {string} [config.chooseFromLibrary.title="Choose from library"] Button title.
+     * @param {object} [config.chooseFromLibrary.config={}] Config objects passed to ImagePickerIOS.openSelectDialog().
+     *
      * @returns {Promise}
      */
     open: function (config) {
@@ -20,36 +35,44 @@ var ImagePicker = {
             var buttons = [];
 
             // extend config with defaults
-            config = Object.assign({
-                cancel: 'Cancel',
-                takePhoto: 'Take Photo',
-                useLastPhoto: 'Use last photo',
-                chooseFromLibrary: 'Choose from library'
-            }, config);
-            config.cancel = config.cancel || 'Cancel';
+            config = config || {};
 
-            addButton('cancel', 'Cancel');
-            addButton('takePhoto', 'Take Photo');
-            addButton('useLastPhoto', 'Use last photo');
-            addButton('chooseFromLibrary', 'Choose from library');
+            config.cancelTitle = config.cancelTitle || 'Cancel';
+            asOptions.push(config.cancelTitle);
+            buttons.push('cancel');
+
+            addButton('takePhoto', {
+                title: 'Take Photo',
+                config: {}
+            });
+
+            addButton('useLastPhoto', {
+                title: 'Use last photo',
+                config: {first: 1}
+            });
+
+            addButton('chooseFromLibrary', {
+                title: 'Choose from library',
+                config: {}
+            });
 
             ActionSheetIOS.showActionSheetWithOptions({
                 options: asOptions,
                 cancelButtonIndex: 0
             }, (index) => {
                 var actionId = buttons[index];
+                var buttonCfg = config[actionId];
 
                 switch (actionId) {
                     case 'takePhoto':
-                        ImagePickerIOS.openCameraDialog({}, resolve, reject);
+                        ImagePickerIOS.openCameraDialog(buttonCfg.config, resolve, reject);
                         break;
 
                     case 'useLastPhoto':
-                        CameraRoll.getPhotos({first: 1}, function (response) {
-                            console.log('getPhotos', 'success', response);
+                        CameraRoll.getPhotos(buttonCfg.config, function (response) {
                             var lastPhoto = response.edges[0];
                             if (lastPhoto) {
-                                resolve(node.image.uri);
+                                resolve(lastPhoto.node.image.uri);
                             } else {
                                 reject('NO_PHOTOS');
                             }
@@ -57,16 +80,34 @@ var ImagePicker = {
                         break;
 
                     case 'chooseFromLibrary':
-                        ImagePickerIOS.openSelectDialog({}, resolve, reject);
+                        ImagePickerIOS.openSelectDialog(buttonCfg.config, resolve, reject);
+                        break;
+
+                    default:
+                        reject();
                         break;
                 }
             });
 
-            function addButton(id, defaultTitle) {
-                if (config[id]) {
-                    config[id] = config[id] || defaultTitle;
+            function addButton(id, defaults) {
+                var button = config[id];
+                if (button) {
 
-                    asOptions.push(config[id]);
+                    // cast to object
+                    var type = typeof button;
+                    if (type === 'string') {
+                        button = {
+                            title: button
+                        };
+
+                    } else if (type !== 'object') {
+                        button = {};
+                    }
+
+                    button = Object.assign(defaults, button);
+                    config[id] = button;
+
+                    asOptions.push(button.title);
                     buttons.push(id);
                 }
             }
